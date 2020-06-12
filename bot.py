878900e908d7b09ef3e1544794c7f9c8f286a58a
Lola <lola@egherman.com>
2020-06-12 11:57:00 -0500

    work with new button format

--- bot.py
+++ bot.py
@@ -18,8 +18,7 @@ chtwrs_id = 408101137
 async def new_message(event):
     if event.from_id == pvebot_id:
         print('Forwarding message from PVE bot')
-        print(event.buttons)
-        await client.send_message(chtwrs_id, event.buttons[0].inline_query)
+        await client.send_message(chtwrs_id, event.buttons[0][0].url.replace('https://t.me/share/url?url=',''))
         await event.mark_read()
     elif event.from_id == chtwrs_id:
         if event.message.message == "Too late. Action is not available.":
